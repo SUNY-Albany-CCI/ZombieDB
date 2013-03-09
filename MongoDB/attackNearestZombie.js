0468def015abf1370f774a124a815dcc6672cf20
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 14:08:14 -0500

    attack refined

--- MongoDB/attackNearestZombie.js
+++ MongoDB/attackNearestZombie.js
@@ -4,13 +4,40 @@
 //    Use your head: cut off theirs.
 //
 
-var person = db.people.findOne( { name: "Sean" });
+var person = db.people.findOne( { name: "Sean" } );
 
-print('\nPerson ', person.name,'\n');
+print('\n',person.name, 'is attacking');
 
 var listOfZombies = db.zombies.find( { location: { $near: person.location } } );
 
 var closestZombie = listOfZombies[0];
 
-print(closestZombie.location);
+var weaponName = person.weapon;
+
+print('with weapon ', weaponName );
+
+var weaponEntry = db.weapons.findOne( { name: weaponName } );
+
+var weaponStrength = weaponEntry.strength;
+
+var remainingStrength = closestZombie.strength - weaponStrength;
+
+if ( remainingStrength > 0 ) {
+
+  db.zombies.update( { _id: closestZombie._id }, { $set :  { strength: remainingStrength } } );
+
+  print('zombie location ', closestZombie.location, 'strength ', remainingStrength);
+
+}
+else {
+
+  print('ZOMBIE DOWN !!');
+
+  db.zombies.remove( { _id: closestZombie._id } );
+
+  print('The zombie may be gone,');
+  print('but the threat lives on...');
+
+}
+
 
