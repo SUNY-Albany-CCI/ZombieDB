51575134d3e5fad6afd67932af66f78cc45cd426
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 10:15:44 -0500

    Print also people names

--- MongoDB/findPeopleNearToPerson.js
+++ MongoDB/findPeopleNearToPerson.js
@@ -2,7 +2,7 @@ db.people.find( { name: "Sean" }).forEach(function(person) {
 
   db.people.find( { location: { $near: person.location } } ).forEach(function(closebyperson) {
 
-    print( closebyperson.location, closebyperson.weapon )
+    print( closebyperson.name, closebyperson.location, closebyperson.weapon )
 
     })
 
