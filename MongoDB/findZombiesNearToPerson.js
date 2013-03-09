980e757b99399e8d00b3fc7235d262f7475c0b40
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 17:51:48 -0500

    Forgot to add labels to first print

--- MongoDB/findZombiesNearToPerson.js
+++ MongoDB/findZombiesNearToPerson.js
@@ -10,7 +10,7 @@ print('\nThe following Zombies are near ', person.name,'\n');
 
 db.zombies.find( { location: { $near: person.location } } ).forEach(function(zombie) {
 
-  print( zombie.location, zombie.strength );
+  print( 'location: ', zombie.location, ' strength: ', zombie.strength );
 
   });
 
