06c67b02763d88464505d4d6f543870efff79355
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 10:22:42 -0500

    added a header

--- MongoDB/listWeapons.js
+++ MongoDB/listWeapons.js
@@ -1,6 +1,9 @@
 //
 // Survival Suggestion: Pick weapons that hurt zombies.
 //
+
+print('\n These are the suggested weapons \n')
+
 db.weapons.find().forEach(function(weapon) {
   print( weapon.name, " effectivenes: ", weapon.effectiveness )
 })
