e35349ce6895dacf528796f102df075eabd7eb95
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 14:21:09 -0500

    computing distance in meters

---
+++ MongoDB/computeDistance.js
@@ -0,0 +1,15 @@
+function distance(pos1, pos2) {
+  var lat1 = pos1[0];
+  var lon1 = pos1[1];
+  var lat2 = pos2[0];
+  var lon2 = pos2[1];
+  var R = 6371; // Radius of the earth in km
+  var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+  var dLon = (lon2-lon1).toRad();
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c * 1000; // Distance in meters
+  return d;
+}
