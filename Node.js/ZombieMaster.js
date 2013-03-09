2f488bf83f8905db8da941839100bb335a861749
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 15:52:57 -0500

    learning to use Cursors

--- Node.js/ZombieMaster.js
+++ Node.js/ZombieMaster.js
@@ -1,36 +1,38 @@
 var http = require('http');
 var mongodb = require('mongodb');
+
 var server = new mongodb.Server("127.0.0.1", 27017, {});
+
 var db = new mongodb.Db('zombiedb', server, {})
+
 var port = 8666;
 
 http.createServer( function(req, res) {
 
-console.log('Server Started ');
-
-db.open(function (error, client) {
-
-  if (error) throw error;
-
-  var collection = new mongodb.Collection(client, 'zombies');
+  console.log('Server Started ');
 
   res.writeHead(200, {'content-type': 'text/plain'});
   res.write('No place is safe, only safer !\n');
 
-  collection.find().forEach( function( zombie ) {
+  db.open( function (error, client) {
 
-        res.write('position ');
-        res.write(zombie.location[0]);
-        res.write(', ');
-        res.write(zombie.location[1]);
-        res.write(' strenght ');
-        res.write(zombie.strenght);
-        res.write('\n');
-    })
+    if (error) throw error;
 
-  res.end("Good Bye !\n");
+    var collection = new mongodb.Collection(client, 'zombies');
 
-  });
+    var listOfZombies = collection.find();
+
+    listOfZombies.each( function(err, zombie ) {
+
+      res.write(' strength ');
+      res.write('\n');
+
+      });
+
+    db.close();
+    });
+
+  res.end("Good Luck !\n");
 
 }).listen(port);
 
