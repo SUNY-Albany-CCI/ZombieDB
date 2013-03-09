9b35aa24fb9528bd32c3a4f05ae8e613e49f29f4
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 13:22:54 -0500

    improve formatting, remove duplicate query

--- MongoDB/listZombies.js
+++ MongoDB/listZombies.js
@@ -7,7 +7,7 @@ print('\nList of zombies... no place is safe... \n');
 
 db.zombies.find().forEach(function(zombie) {
 
-  print(zombie._id, " strength: ", zombie.strength, " location: ",zombie.location );
+  print(zombie._id, " strength: ", zombie.strength, " location: ", zombie.location );
 
 });
 
