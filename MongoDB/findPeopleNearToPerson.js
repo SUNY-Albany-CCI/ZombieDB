45eca4e19fab367e020f46ff9b8e3300198d76c6
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-09 13:17:57 -0500

    Added message about person name

--- MongoDB/findPeopleNearToPerson.js
+++ MongoDB/findPeopleNearToPerson.js
@@ -1,7 +1,10 @@
 //
 //  Survival Advice:  Organize before they rise !
 //
+
 db.people.find( { name: "Sean" }).forEach(function(person) {
+
+  print('\nThe following people are near ', person.name,'\n');
 
   db.people.find( { location: { $near: person.location } } ).forEach(function(closebyperson) {
 
--- MongoDB/findWeaponsNearToPerson.js
+++ MongoDB/findWeaponsNearToPerson.js
@@ -3,6 +3,8 @@
 //
 db.people.find( { name: "Sean" }).forEach(function(person) {
 
+  print('\nThe following weapons are near ', person.name,'\n');
+
   db.weapons.find( { location: { $near: person.location } } ).forEach(function(weapon) {
 
     print( weapon.name, weapon.location );
