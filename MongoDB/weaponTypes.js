181aae3430662bccd06c9cad6738163eafdfe6cc
Luis Ibanez <luis.ibanez@kitware.com>
2013-03-10 19:50:29 -0400

    Added weapon types with matching strength

---
+++ MongoDB/weaponTypes.js
@@ -0,0 +1,32 @@
+//
+//
+// ...Chosing the right weapons (never carry just one) can make the difference
+// between a pile of dead zombies and becoming one yourself. When confronted
+// with the undead, it is easy to believe in the supper commando strategy: Load
+// up with the heaviest, most powerfulweapons possible and go out to "kick
+// ass." This is not only foolish-- it is suicidal...
+//
+//                                 The Zombie Survival Guide. Page 28.
+//
+//
+db.weapons.insert( { name : "Baseball Bat", strength: 10, location: [0,0] } );
+db.weapons.insert( { name : "Sledgehammer", strength:  6, location: [0,0] } );
+db.weapons.insert( { name : "Hammer",       strength:  7, location: [0,0] } );
+db.weapons.insert( { name : "Lead Pipe",    strength:  5, location: [0,0] } );
+db.weapons.insert( { name : "Crowbar",      strength: 12, location: [0,0] } );
+db.weapons.insert( { name : "Ax",           strength:  8, location: [0,0] } );
+db.weapons.insert( { name : "Sword",        strength: 10, location: [0,0] } );
+db.weapons.insert( { name : "Katana",       strength: 15, location: [0,0] } );
+db.weapons.insert( { name : "Machete",      strength:  9, location: [0,0] } );
+db.weapons.insert( { name : "Spear",        strength:  3, location: [0,0] } );
+db.weapons.insert( { name : "Trident",      strength:  2, location: [0,0] } );
+db.weapons.insert( { name : "Hachet",       strength:  8, location: [0,0] } );
+db.weapons.insert( { name : "Knife",        strength:  6, location: [0,0] } );
+db.weapons.insert( { name : "Shaolin Spade",strength: 20, location: [0,0] } );
+db.weapons.insert( { name : "Chainsaw",     strength:  1, location: [0,0] } );
+db.weapons.insert( { name : "Power Drill",  strength:  1, location: [0,0] } );
+db.weapons.insert( { name : "Sling",        strength:  4, location: [0,0] } );
+db.weapons.insert( { name : "Slingshot",    strength:  1, location: [0,0] } );
+db.weapons.insert( { name : "Crossbow",     strength:  9, location: [0,0] } );
+db.weapons.insert( { name : "Blowtorch",    strength:  3, location: [0,0] } );
+
